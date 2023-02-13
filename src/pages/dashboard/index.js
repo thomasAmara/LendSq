5f4adf200e1fb82214c84212630b4cbd139742d3
thomasAmara <thomastarawali@gmail.com>
2023-02-13 11:43:57 +0000

    table header icons

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -7,6 +7,9 @@ import {
   useMediaQuery,
   Stack,
   Button,
+  Icon,
+  Flex,
+  useDisclosure,
 } from '@chakra-ui/react';
 import { Table, Pagination, Popover } from 'antd';
 import { useSelector, useDispatch } from 'react-redux';
@@ -15,6 +18,7 @@ import { view } from '../../../components/Reducers/customerReducer/Action';
 import More from '../../../public/images/more.png';
 import Link from 'next/link';
 import moment from 'moment';
+import { UseMediaQuery } from '../../../components/MediaQuery';
 import Saving from '../../../public/images/userSaving.svg';
 import Active from '../../../public/images/userIcons.svg';
 import Report from '../../../public/images/loanReport.svg';
@@ -22,11 +26,14 @@ import User from '../../../public/images/activeUsers.svg';
 import ActivateUser from '../../../public/images/activateUser.svg';
 import Eye from '../../../public/images/Eye.svg';
 import Blacklist from '../../../public/images/blacklistUser.svg';
-// import Filter from '../../../public/images/filterIcon.svg';
+import Filter from '../../../public/images/filterIcon.svg';
+import FilterModal from 'components/Modal/Filter';
 
 function Dashboard() {
   const [id, setID] = React.useState('');
   const [data, setData] = React.useState([]);
+  const isLargerThan480 = UseMediaQuery(480);
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const dispatch = useDispatch();
 
   const [isLargerThan768] = useMediaQuery('(min-width: 768px)');
@@ -49,30 +56,56 @@ function Dashboard() {
 
   const columns = [
     {
-      //   title: ('<>
-      //   <Filter /> 546
-      // </> ')
-      //    ,
-      title: (filters, sortOrder) => <div>Title</div>,
+      title: () => (
+        <Flex
+          cursor='pointer'
+          alignItems='center'
+          justifyContent='space-evenly'
+          onClick={onOpen}
+        >
+          <Text ml='20px'>ORGANISATION</Text>
+          <Image src={Filter} width='30px' height='30px' alt='' />
+        </Flex>
+      ),
       dataIndex: 'orgName',
       key: 'name',
     },
     {
-      title: 'USER',
+      title: () => (
+        <Flex>
+          <Text>USERNAME</Text>
+          <Image src={Filter} width='30px' height='30px' alt='' />
+        </Flex>
+      ),
       dataIndex: 'userName',
     },
     {
-      title: 'EMAIL',
+      title: () => (
+        <Flex>
+          <Text>EMAIL</Text>
+          <Image src={Filter} width='30px' height='30px' alt='' />
+        </Flex>
+      ),
       dataIndex: 'email',
       width: 100,
     },
     {
-      title: 'PHONE NUMBER',
+      title: () => (
+        <Flex>
+          <Text>PHONE NUMBER</Text>
+          <Image src={Filter} width='30px' height='30px' alt='' />
+        </Flex>
+      ),
       dataIndex: 'phoneNumber',
       render: (item) => Object.values(item),
     },
     {
-      title: 'DATE JOINED',
+      title: () => (
+        <Flex>
+          <Text>DATE JOINED</Text>
+          <Image src={Filter} width='30px' height='30px' alt='' />
+        </Flex>
+      ),
       dataIndex: 'createdAt',
       render: (value) => {
         return <span>{moment(value).format('lll')}</span>;
@@ -134,19 +167,21 @@ function Dashboard() {
   ];
   return (
     <DashboardLayout>
-      <Box className='home' bg='#E5E5E5' p='20px 20px'>
+      <Box className='home' bg='#E5E5E5' p='24px 20px 15px'>
         <Box pl='40px' pb='20px' mb='20px'>
           <Text fontSize='24px' fontWeight='500' color='#21357D'>
             USER
           </Text>
-          <Box mt='10px'>
-            <Stack justify='space-between' direction={['column', 'row']}>
+          <Box mt='30px'>
+            {/* <Stack spacing={7} flexWrap='wrap' direction={['column', 'row']}> */}
+            <Wrap spacing='30px'>
               {userData.map((data, index) => {
                 return (
                   <Box
                     className='customerContainer'
-                    border='1px solid blue'
+                    // border='1px solid blue'
                     key={index}
+                    maxWidth={isLargerThan480 ? '240px' : '320px'}
                   >
                     <Box
                       bg={
@@ -200,7 +235,8 @@ function Dashboard() {
                   </Box>
                 );
               })}
-            </Stack>
+            </Wrap>
+            {/* </Stack> */}
             <Box mt='20px' mb='20px'>
               <Table
                 size='small'
@@ -227,9 +263,23 @@ function Dashboard() {
           </Box>
         </Box>
       </Box>
+      <FilterModal isOpen={isOpen} onClose={onClose} />
     </DashboardLayout>
   );
 }
+
+const styles = {
+  filterBox: {
+    border: ' 1px solid #E5EBF0',
+    borderRadius: '4px',
+    color: '#2D4047',
+    paddingLeft: '34px',
+    fontSize: '14px',
+  },
+  title: {
+    fontWeight: 'bold',
+  },
+};
 
 const userData = [
   { title: 'User', value: 2453, image: Active },
